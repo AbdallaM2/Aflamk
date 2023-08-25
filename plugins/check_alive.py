6124dfdc1fb214ea7a4149c2d06771d89e521ef9
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-08-25 22:43:35 +0530

    Create check_alive.py

---
+++ plugins/check_alive.py
@@ -0,0 +1,11 @@
+import time
+import random
+from pyrogram import Client, filters
+
+@Client.on_message(filters.new_chat_members)
+async def welcome(bot, message):
+	await message.delete()	
+	
+@Client.on_message(filters.left_chat_member)
+async def goodbye(bot, message):
+	await message.delete()
