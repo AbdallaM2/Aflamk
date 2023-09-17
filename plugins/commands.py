ff27fff3e8e23a6ba448c5b7a65dd0ffb9c41ea7
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-09-17 21:31:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -264,8 +264,8 @@ async def start(client, message):
             pass
         return await message.reply('No such file exist.')
     files = files_[0]
-    title = '@TeamHMT ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), file.file_name.split()))
-    size=get_size(file.file_size)
+    title = '@TeamHMT ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))
+    size=get_size(files.file_size)
     f_caption=files.caption
     if CUSTOM_FILE_CAPTION:
         try:
